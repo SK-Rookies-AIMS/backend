e0a8367140de2112853391af82633a78a6b0dde4
이준호 <91dlwnsgh@naver.com>
2026-06-18 14:02:38 +0900

    사이드바 오늘 실적 및 생산 추이 조회 기능 구현

--- src/main/java/com/aims/backend/service/MainPageService.java
+++ src/main/java/com/aims/backend/service/MainPageService.java
@@ -1,12 +1,16 @@
 package com.aims.backend.service;
 
+import com.aims.backend.domain.mainpage.InspectionSummary;
 import com.aims.backend.domain.mainpage.UserTask;
 import com.aims.backend.dto.mainpage.UserTaskResponse;
+import com.aims.backend.dto.mainpage.WeeklyProductionResponse;
 import com.aims.backend.repository.UserTaskRepository;
+import com.aims.backend.repository.mainpage.InspectionSummaryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,6 +21,7 @@ import java.util.stream.Collectors;
 public class MainPageService {
 
     private final UserTaskRepository mainPageUserTaskRepository;
+    private final InspectionSummaryRepository inspectionSummaryRepository;
 
     public List<UserTaskResponse.MainPageTaskDTO> getUserTasks(Long userId) {
         LocalDateTime currentTime = LocalDateTime.now()
@@ -32,5 +37,14 @@ public class MainPageService {
                         .scheduledAt(task.getScheduledAt())
                         .build())
                 .collect(Collectors.toList());
+    }
+
+    public List<WeeklyProductionResponse> getWeeklyProductionTrend() {
+        LocalDateTime endDate = LocalDate.now().atStartOfDay().plusDays(1).minusNanos(1); // End of today
+        LocalDateTime startDate = endDate.minusDays(7).withHour(0).withMinute(0).withSecond(0).withNano(0); // 6 days ago from today, start of the day
+
+        List<InspectionSummary> inspectionSummaries = inspectionSummaryRepository.findByCreatedAtBetweenAndTotalCount(startDate, endDate, 100);
+
+        return WeeklyProductionResponse.fromInspectionSummaries(inspectionSummaries);
     }
 }
